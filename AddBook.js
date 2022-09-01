1a05e64ce4867e3a84eb1c59dab8c07300805b3e
Jahnavi3003 <sujayithimulkj30.com>
2022-09-01 15:10:02 +0530

    Checking duplicate contact

--- AddBook.js
+++ AddBook.js
@@ -1,9 +1,7 @@
-console.log(" WELCOME TO ADDRESS BOOK PROBLEM ");
-
 class Contact{
-   
-    first_name;
-    last_name;
+    
+    firstName;
+    lastName;
     address;
     city;
     state;
@@ -12,9 +10,9 @@ class Contact{
     email;
 
     
-    constructor(first_name , last_name , address , city , state , zip_code , phone_number , email){
-        this.first_name = first_name;
-        this.last_name = last_name;
+    constructor(firstName , lastName , address , city , state , zip_code , phone_number , email){
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.address = address;
         this.city = city;
         this.state = state;
@@ -24,6 +22,7 @@ class Contact{
     }
 }
 
+
 const PATTERN_NAME = /^[A-Z][A-Z a-z]{3,}$/;
 const PATTERN_ADDRESS = /^[A-Z][A-Z a-z]{4,}$/;
 const PATTERN_CITY = /^[A-Z][A-Z a-z]{4,}$/;
@@ -32,29 +31,97 @@ const PATTERN_ZIP = /^[\d]{3}[\s]?[\d]{3}$/;
 const PATTERN_PHONE = /^[\d]{2}(\s){1}[\d]{10}$/;
 const PATTERN_EMAIL = /^[\w+-]+(\.[\w+-]+)*@[\w]+(\.[\w]+)?(?=(\.[A-Za-z_]{2,3}$|\.[a-zA-Z]{2,3}$)).*$/;
 
+
 var addressBook = new Array();
 
+
 function addContacts(firstName , lastName , address , city , state , zip_code , phone_number , email){ 
-    let result_firstName = PATTERN_NAME.test(firstName); 
-    let result_lastName = PATTERN_NAME.test(lastName);
-    let result_address = PATTERN_ADDRESS.test(address);
-    let result_city = PATTERN_CITY.test(city);
-    let result_state = PATTERN_STATE.test(state);
-    let result_zip = PATTERN_ZIP.test(zip_code);
-    let result_phoneNum = PATTERN_PHONE.test(phone_number);
-    let result_email = PATTERN_EMAIL.test(email);
-
-    if(result_firstName == true && result_lastName == true && result_address == true && result_city == true && 
-       result_state == true && result_zip == true && result_phoneNum == true && result_email == true){
-
-        let contact = new Contact(firstName , lastName , address , city , state , zip_code , phone_number , email);
-
-        console.log("Contact validated successfully.")
+    
+  
+    addressBook.filter(person => person.firstName === firstName)
+    .reduce( () => countPersons++ , countPersons = 0);
+    if(countPersons > 0){
+        console.log("OOPS! Contact with same first name as " + firstName + " is already exists.");
     }
     else{
-         "Please enter only valid contact details.";     
+        let result_firstName = PATTERN_NAME.test(firstName); 
+        let result_lastName = PATTERN_NAME.test(lastName);
+        let result_address = PATTERN_ADDRESS.test(address);
+        let result_city = PATTERN_CITY.test(city);
+        let result_state = PATTERN_STATE.test(state);
+        let result_zip = PATTERN_ZIP.test(zip_code);
+        let result_phoneNum = PATTERN_PHONE.test(phone_number);
+        let result_email = PATTERN_EMAIL.test(email);
+
+        if(result_firstName == true && result_lastName == true && result_address == true && result_city == true && 
+        result_state == true && result_zip == true && result_phoneNum == true && result_email == true){
+
+            let contact = new Contact(firstName , lastName , address , city , state , zip_code , phone_number , email);
+
+            addressBook.push(contact);   
+        }
+        else{
+            throw "Please enter only valid contact details.";     
+        }
     }
 }
+
+/** Editing contacts by using first name. */
+function editContact(first_name , option , newValue){
+    if(addressBook.length == 0){
+        console.log("OOPS! Address Book is empty.");
+    }
+    else{
+        addressBook.forEach(contact => {
+            if(contact.firstName == first_name){  
+                switch(option){
+                    case "firstName":
+                        contact.firstName = newValue;
+                        break;
+                    case "lastName":
+                        contact.lastName = newValue;
+                        break;
+                    case "address":
+                        contact.address - newValue;
+                        break;
+                    case "city":
+                        contact.city = newValue;
+                        break;
+                    case "state":
+                        contact.state = newValue;
+                        break;                   
+                    case "zipCode":
+                        contact.zip_code = newValue;
+                        break;
+                    case "phoneNum":
+                        contact.phone_number = newValue;
+                        break;
+                    case "email":
+                        contact.email = newValue;
+                        break;
+                    default:
+                        console.log("Please choose valid option.");
+                }
+            }
+        });
+    }
+}
+
+
+function deleteContact(first_name){
+    if(addressBook.length == 0){
+        console.log("OOPS! Address Book is empty.");
+    }
+    else{
+        for(let i = 0; i < addressBook.length; i++){
+            if(addressBook[i].firstName === first_name){    
+                addressBook.splice(i , 1);
+                console.log("Successfully deleted contact with first name as " + first_name + " .");
+            }
+        }
+    }
+}
+
 
 function numberOfContacts(){
     addressBook.reduce( () => {
@@ -63,9 +130,9 @@ function numberOfContacts(){
     console.log("Number of contacts in Address Book :- " + count);
 }
 
-addContacts("lavi", "sankar","mtm","pradesh","AP",500001,8135455842,"lavidavi@gmail.com");
 
-addContacts("Gum","Nahani","Mangalgiri","Telangana","India",520441,851233142,"gumghor@gmail.com");
-
+addContacts("Nikhil","Sundarasetty","Pellur","Ongole","Andhra pradesh",523272,"91 9398359637","nikhilnaidu473@gmail.com");
+addContacts("Bill","Gates","Newyork","America","America","400 008","91 9258566655","bill-gates@gmail.com");
 console.log(addressBook);
-numberOfContacts();
+
+addContacts("Elon","Musk","StarBase","America","America",400007,"91 5555588822","elon-musk125@gmail.com");
