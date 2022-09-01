ac589efa7235274cf2df124724139522839e9908
Jahnavi3003 <sujayithimulkj30.com>
2022-09-01 15:31:55 +0530

    Search person in city or state

--- AddBook.js
+++ AddBook.js
@@ -1,7 +1,9 @@
+console.log(" WELCOME TO ADDRESS BOOK PROBLEM ");
+
 class Contact{
-    
-    firstName;
-    lastName;
+   
+    first_name;
+    last_name;
     address;
     city;
     state;
@@ -10,9 +12,9 @@ class Contact{
     email;
 
     
-    constructor(firstName , lastName , address , city , state , zip_code , phone_number , email){
-        this.firstName = firstName;
-        this.lastName = lastName;
+    constructor(first_name , last_name , address , city , state , zip_code , phone_number , email){
+        this.first_name = first_name;
+        this.last_name = last_name;
         this.address = address;
         this.city = city;
         this.state = state;
@@ -22,7 +24,6 @@ class Contact{
     }
 }
 
-
 const PATTERN_NAME = /^[A-Z][A-Z a-z]{3,}$/;
 const PATTERN_ADDRESS = /^[A-Z][A-Z a-z]{4,}$/;
 const PATTERN_CITY = /^[A-Z][A-Z a-z]{4,}$/;
@@ -31,42 +32,30 @@ const PATTERN_ZIP = /^[\d]{3}[\s]?[\d]{3}$/;
 const PATTERN_PHONE = /^[\d]{2}(\s){1}[\d]{10}$/;
 const PATTERN_EMAIL = /^[\w+-]+(\.[\w+-]+)*@[\w]+(\.[\w]+)?(?=(\.[A-Za-z_]{2,3}$|\.[a-zA-Z]{2,3}$)).*$/;
 
-
 var addressBook = new Array();
 
-
 function addContacts(firstName , lastName , address , city , state , zip_code , phone_number , email){ 
-    
-  
-    addressBook.filter(person => person.firstName === firstName)
-    .reduce( () => countPersons++ , countPersons = 0);
-    if(countPersons > 0){
-        console.log("OOPS! Contact with same first name as " + firstName + " is already exists.");
+    let result_firstName = PATTERN_NAME.test(firstName); 
+    let result_lastName = PATTERN_NAME.test(lastName);
+    let result_address = PATTERN_ADDRESS.test(address);
+    let result_city = PATTERN_CITY.test(city);
+    let result_state = PATTERN_STATE.test(state);
+    let result_zip = PATTERN_ZIP.test(zip_code);
+    let result_phoneNum = PATTERN_PHONE.test(phone_number);
+    let result_email = PATTERN_EMAIL.test(email);
+
+    if(result_firstName == true && result_lastName == true && result_address == true && result_city == true && 
+       result_state == true && result_zip == true && result_phoneNum == true && result_email == true){
+
+        let contact = new Contact(firstName , lastName , address , city , state , zip_code , phone_number , email);
+
+        console.log("Contact validated successfully.")
     }
     else{
-        let result_firstName = PATTERN_NAME.test(firstName); 
-        let result_lastName = PATTERN_NAME.test(lastName);
-        let result_address = PATTERN_ADDRESS.test(address);
-        let result_city = PATTERN_CITY.test(city);
-        let result_state = PATTERN_STATE.test(state);
-        let result_zip = PATTERN_ZIP.test(zip_code);
-        let result_phoneNum = PATTERN_PHONE.test(phone_number);
-        let result_email = PATTERN_EMAIL.test(email);
-
-        if(result_firstName == true && result_lastName == true && result_address == true && result_city == true && 
-        result_state == true && result_zip == true && result_phoneNum == true && result_email == true){
-
-            let contact = new Contact(firstName , lastName , address , city , state , zip_code , phone_number , email);
-
-            addressBook.push(contact);   
-        }
-        else{
-            throw "Please enter only valid contact details.";     
-        }
+        throw "Please enter only valid contact details.";     
     }
 }
 
-/** Editing contacts by using first name. */
 function editContact(first_name , option , newValue){
     if(addressBook.length == 0){
         console.log("OOPS! Address Book is empty.");
@@ -89,8 +78,7 @@ function editContact(first_name , option , newValue){
                         break;
                     case "state":
                         contact.state = newValue;
-                        break;                   
-                    case "zipCode":
+                        break;                    case "zipCode":
                         contact.zip_code = newValue;
                         break;
                     case "phoneNum":
@@ -107,32 +95,27 @@ function editContact(first_name , option , newValue){
     }
 }
 
-
-function deleteContact(first_name){
-    if(addressBook.length == 0){
-        console.log("OOPS! Address Book is empty.");
+function searchPersonByCityOrState(choice , cityOrState){
+    if(choice == "city" || choice == "City" || choice == "CITY"){
+        person = addressBook.filter(contact => contact.city === cityOrState)
+        .map(contact => contact.firstName);
+        console.log(person);
+    }
+    else if(choice == "state" || choice == "State" || choice == "STATE"){
+        person = addressBook.filter(contact => contact.state === cityOrState)
+        .map(contact => contact.firstName);
+        console.log(person);
     }
     else{
-        for(let i = 0; i < addressBook.length; i++){
-            if(addressBook[i].firstName === first_name){    
-                addressBook.splice(i , 1);
-                console.log("Successfully deleted contact with first name as " + first_name + " .");
-            }
-        }
+        console.log("OOPS! Please choose city or state only to search.");
     }
 }
 
+addContacts("lavi", "sankar","mtm","pradesh","AP",500001,8135455842,"lavidavi@gmail.com");
 
-function numberOfContacts(){
-    addressBook.reduce( () => {
-        count++;
-    },count = 0);
-    console.log("Number of contacts in Address Book :- " + count);
-}
+addContacts("Gum","Nahani","Mangalgiri","Telangana","India",520441,851233142,"gumghor@gmail.com");
 
-
-addContacts("Nikhil","Sundarasetty","Pellur","Ongole","Andhra pradesh",523272,"91 9398359637","nikhilnaidu473@gmail.com");
-addContacts("Bill","Gates","Newyork","America","America","400 008","91 9258566655","bill-gates@gmail.com");
 console.log(addressBook);
 
-addContacts("Elon","Musk","StarBase","America","America",400007,"91 5555588822","elon-musk125@gmail.com");
+editContact("Gum","state","Goa");
+console.log(addressBook);
